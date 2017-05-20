67595f6303dbd2f4cfd2ec66d524c205479d57f6
Grant Merola <grantmerola@gmail.com>
2017-05-20 11:37:53 -0700

    Colors and fixes
    Added colors, fixed mobile version, finished build script, note
    minified version 👎

--- html:css/filesystems/slimbuild.sh
+++ html:css/filesystems/slimbuild.sh
@@ -1,7 +1,5 @@
 # !bin/bash
-#markdown filesystems.md | place in slim file
 slimrb -p filesystems.slim > index.html
-open -a "Google Chrome" index.html
-slimrb filesystems.slim > index_min.html
-echo "index_min size"
-wc -c index_min.html
+markdown filesystems.md > filesystems.html
+sed -i '' -e '/REPLACE/r filesystems.html' -e '/REPLACE/d' index.html
+#open -a "Google Chrome" index.html
